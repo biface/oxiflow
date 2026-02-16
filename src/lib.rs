76811f7a8eda05e4355743daae081a94dfae97ae
biface <fabrice.moutte.wn@gmail.com>
2026-02-16 12:55:16 +0100

    chores:init project

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! # Oxiflow
+//!
+//! **⚠️ WORK IN PROGRESS ⚠️**
+//!
+//! Modular reactive transport solver for PDEs.
+
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub fn info() -> &'static str {
+    "Oxiflow v0.0.1 - Placeholder release"
+}
