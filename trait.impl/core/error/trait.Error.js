0643de20b9b2d20d91bd7fbf90bd5cc16c6ca2e0
biface <biface@users.noreply.github.com>
2026-06-20 11:17:44 +0000

    deploy: 2f2c7bbc732a5c99dfd1d1c5b77ea3636e56041c

--- trait.impl/core/error/trait.Error.js
+++ trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["oxiflow",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"oxiflow/context/error/enum.OxiflowError.html\" title=\"enum oxiflow::context::error::OxiflowError\">OxiflowError</a>",0]]]]);
+    const implementors = Object.fromEntries([["oxiflow",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"oxiflow/context/error/enum.OxiflowError.html\" title=\"enum oxiflow::context::error::OxiflowError\">OxiflowError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
